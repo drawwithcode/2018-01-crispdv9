b060a334d2fd71d6e912c1a8463f9a25633e3983
Cristina <crispv9@hotmail.com>
2018-09-24 11:43:49 +0200

    Robot

--- sketch.js
+++ sketch.js
@@ -10,14 +10,36 @@ function draw() {
 	background(204);
 
 	//Neck
-	line(250, 285, 250, 345);
-
+	strokeWeight(2)
+	line(260, 280, 260, 340);
+	line(270,275,270,335);
+	line(280,270,280,330);
 	//Wheel
+	strokeWeight(0)
+	fill(96,94,96)
 	ellipse(250, 465, 70, 70);
-
 	//Body
+	fill(0,0,0)
 	rect(200, 325, 100, 140);
-
+	fill(96,94,96)
+	rect(200,335,100,8);
 	//Head
-	ellipse(250, 240, 90, 90);
+	strokeWeight(2);
+  stroke(96,94,96,60);
+  line(265,235,235,200)
+  line(290,130,280,200);
+  line(335,260,290,245)
+	fill(0,0,0)
+	ellipse(270, 240, 90, 90);
+	fill(255,255,255);
+	ellipse(283,233,28);
+	fill(0,0,0);
+	ellipse(283,233,5);
+	fill(96,94,96);
+	ellipse(258,230,10);
+	fill(96,94,96);
+	ellipse(295,215,8);
+	fill(96,94,96);
+	ellipse(300,245,6);
+
 }
