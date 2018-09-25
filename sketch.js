7bf00ccc366aa2ffb79674988d7428cc8ed06764
Cristina <crispv9@hotmail.com>
2018-09-25 16:33:39 +0200

    Robot

--- sketch.js
+++ sketch.js
@@ -13,10 +13,10 @@ function draw() {
 	fill(0,179,48)
 	rect(0,450,500,500)
 	//sole
-	fill(252,204,40)
-	ellipse(100,100,60)
 	fill(252,204,40,100)
-	ellipse(98,100,90)
+	ellipse(mouseX,mouseY,90,90)
+	fill(252,204,40)
+	ellipse(mouseX,mouseY,60,60)
 	//Neck
 	strokeWeight(3)
 	line(260, 280, 260, 340);
@@ -39,18 +39,25 @@ function draw() {
 	fill(96,94,96)
 	ellipse(350,300,25)
 	fill(228,219,26)
+	strokeWeight(2);
+	stroke(0,0,0)
 	rect(200, 325, 100, 140);
 	fill(96,94,96)
 	rect(200,335,100,8);
 	//Head
 	strokeWeight(3);
-  stroke(96,94,96,60);
-  line(265,235,235,200)
+  stroke(0,0,0);
   line(290,130,280,200);
-  line(335,260,290,245)
-	strokeWeight(0);
-	fill(228,219,26)
-	ellipse(270, 240, 90, 90);
+	stroke(96,94,96);
+	line(310,120,320,100);
+	stroke(96,94,96);
+	line(295,120,298,110);
+	stroke(96,94,96);
+	line(280,120,270,100);
+	strokeWeight(2);
+	stroke(0,0,0);
+	fill(228,219,26);
+	rect(185, 200, 150, 90);
 	fill(255,255,255);
 	ellipse(283,233,28);
 	fill(0,0,0);
@@ -61,5 +68,5 @@ function draw() {
 	ellipse(295,215,8);
 	fill(96,94,96);
 	ellipse(300,245,6);
-
+	rect(220,260,40,20);
 }
